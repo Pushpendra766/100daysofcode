e2ec37c5fc65bbbc5dc47942dcb9b679c69cab6e
Pushpendra Chandravanshi <56391001+Pushpendra766@users.noreply.github.com>
2021-04-05 15:07:52 +0530

    Create higher-lower.py

---
+++ Day 14/higher-lower.py
@@ -0,0 +1,46 @@
+from art import logo,vs
+from game_data import data
+import random
+from replit import clear
+
+def pick_accounts():
+  return random.choice(data)
+
+def compare(choice_A,choice_B,choice):
+  if choice_A >= choice_B and choice == 'A':
+    return True
+  elif choice_A <= choice_B and choice == 'B':
+    return True
+   
+
+def update_score(ans,score):
+  if ans == True:
+    print(logo)
+    print(f"You're right! Current score: {score}")
+    return False
+  else:
+    print(logo)
+    print(f"Sorry, that's wrong. Final score: {score}")
+    return True
+
+score = 0
+end_of_game = False 
+print(logo)
+choice_A = pick_accounts()
+
+while not end_of_game :
+  
+  choice_B = pick_accounts()
+
+  while choice_A == choice_B:
+     choice_B = pick_accounts()
+  print(f"Compare A : {choice_A['name']}, a {choice_A['description']}, from {choice_A['country']}.")
+  print(vs)
+  print(f"Against B : {choice_B['name']}, a {choice_B['description']}, from {choice_B['country']}.")
+  choice = (input("Who has more followers? Type 'A' or 'B': ")).upper()
+  ans = compare(choice_A['follower_count'],choice_B['follower_count'],choice)
+  if(ans == True):
+    score += 1
+    choice_A = choice_B
+  clear()
+  end_of_game = update_score(ans,score)
